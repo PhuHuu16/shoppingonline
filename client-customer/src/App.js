614f71e2adf9804ef5bd572d18524a709ba15152
PhuHuu16 <127402913+PhuHuu16@users.noreply.github.com>
2024-07-16 21:43:33 +0700

    Add files via upload

---
+++ client-customer/src/App.js
@@ -0,0 +1,24 @@
+//CLI: npm install axios --save
+import axios from 'axios';
+import React, { Component } from 'react';
+import './App.css';
+import Main from './components/MainComponent';
+import { BrowserRouter } from 'react-router-dom';
+import MyProvider from './contexts/MyProvider';
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+
+class App extends Component {
+  render() {
+    return (
+      <MyProvider>
+        <BrowserRouter>
+          <Main />
+        </BrowserRouter>
+      </MyProvider>
+    );
+  }
+}
+
+
+export default App;
